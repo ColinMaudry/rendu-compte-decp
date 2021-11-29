990e8a76405a559dff25cf0c9c5d3204d5cde05d
Colin Maudry <colin@maudry.com>
2021-11-29 12:14:07 +0100

    Meilleur gestion des en-tête et des montants

--- process.sh
+++ process.sh
@@ -25,10 +25,12 @@ message "+ + + + + + + +"
 
 message "Conversion ISO8859-1 vers UTF-8..."
 
-#iconv -f ISO8859-1 -t UTF-8 $oriCSV -o $csv
+# N'activer iconv que pour un CSV encodé en ISO8859-1
+# iconv -f ISO8859-1 -t UTF-8 $oriCSV -o $csv.utf8
+# mv $csv.utf8 $csv
 
 message "Normalisation des en-têtes de colonnes..."
-head -n 1  $csv | tr -d " ()/" | tr "é" "e" | tr "ô" "o" > temp
+head -n 1  $csv | tr -d " ()/" | sed 's/é/e/g'| tr "ô" "o" | tr "." "_" > temp
 head -n 1 temp
 tail -n +2 $csv >> temp
 mv temp $csv
@@ -41,31 +43,32 @@ message "Conversion du CSV vers un XML basique..."
 echo '<?xml version="1.0" encoding="UTF-8"?>' > $xml
 echo '<csv>' >> $xml
 
-while IFS="|" read -r Annee id acheteur_id acheteur_nom nature objet codeCPV procedure lieuExecution_code lieuExecution_typeCode lieuExecution_nom dureeMois dateNotification datePublicationDonnees montant formePrix titulaires_id titulaires_typeIdentifiant titulaires_denominationLegale
+while IFS="|" read -r Annee id acheteur_id acheteur_nom nature objet codeCPV procedure LieuExecution_code LieuExecution_typeCode lieuExecution_nom DureeMois dateNotification datePublicationDonnees montant formePrix titulaires_id titulaires_typeIdentifianttype_identifiant titulaires_denominationSociale
 
 do
     if [[ ! $id = "id" ]]
     then
         echo "  <marche>" >> $xml
         echo "    <Annee>${Annee}</Annee>" >> $xml
-        echo "    <id>$id</id>" >> $xml
-        echo "    <acheteur_id>$acheteur_id</acheteur_id>" | tr -d " " >> $xml
-        echo "    <acheteur_nom>$acheteur_nom</acheteur_nom>" >> $xml
-        echo "    <nature>$nature</nature>" >> $xml
-        echo "    <objet>$objet</objet>" | sed 's/&/&amp;/g' >> $xml
-        echo "    <codeCPV>$codeCPV</codeCPV>" >> $xml
-        echo "    <procedure>$procedure</procedure>" >> $xml
-        echo "    <lieuExecution_code>$lieuExecution_code</lieuExecution_code>" >> $xml
-        echo "    <lieuExecution_typeCode>$lieuExecution_typeCode</lieuExecution_typeCode>" >> $xml
-        echo "    <lieuExecution_nom>$lieuExecution_nom</lieuExecution_nom>" >> $xml
-        echo "    <dureeMois>$dureeMois</dureeMois>" >> $xml
-        echo "    <dateNotification>$dateNotification</dateNotification>" >> $xml
-        echo "    <datePublicationDonnees>$datePublicationDonnees</datePublicationDonnees>" >> $xml
-        echo "    <montant>$montant</montant>" | tr -d ", " >> $xml
-        echo "    <formePrix>$formePrix</formePrix>" >> $xml
-        echo "    <titulaires_id>$titulaires_id</titulaires_id>" >> $xml
-        echo "    <titulaires_typeIdentifiant>$titulaires_typeIdentifiant</titulaires_typeIdentifiant>" >> $xml
-        echo "    <titulaires_denominationLegale>$titulaires_denominationLegale</titulaires_denominationLegale>" >> $xml
+        echo "    <id>${id}</id>" >> $xml
+        echo "    <acheteur_id>${acheteur_id}</acheteur_id>" | tr -d " " >> $xml
+        echo "    <acheteur_nom>${acheteur_nom}</acheteur_nom>" >> $xml
+        echo "    <nature>${nature}</nature>" >> $xml
+        echo "    <objet>${objet}</objet>" | sed 's/&/&amp;/g' >> $xml
+        echo "    <codeCPV>${codeCPV}</codeCPV>" >> $xml
+        echo "    <procedure>${procedure}</procedure>" >> $xml
+        echo "    <lieuExecution_code>${LieuExecution_code}</lieuExecution_code>" >> $xml
+        echo "    <lieuExecution_typeCode>${lieuExecution_typeCode}</lieuExecution_typeCode>" >> $xml
+        echo "    <lieuExecution_nom>${lieuExecution_nom}</lieuExecution_nom>" >> $xml
+        echo "    <dureeMois>${DureeMois}</dureeMois>" >> $xml
+        echo "    <dateNotification>${dateNotification}</dateNotification>" >> $xml
+        echo "    <datePublicationDonnees>${datePublicationDonnees}</datePublicationDonnees>" >> $xml
+        # Le sed ci-dessous supprime les espaces insécables (non-breaking spaces, NBSP) des montants
+        echo "    <montant>${montant}</montant>" | sed 's/ //g' | sed 's/,/./' >> $xml
+        echo "    <formePrix>${formePrix}</formePrix>" >> $xml
+        echo "    <titulaires_id>${titulaires_id}</titulaires_id>" >> $xml
+        echo "    <titulaires_typeIdentifiant>${titulaires_typeIdentifiant}</titulaires_typeIdentifiant>" >> $xml
+        echo "    <titulaires_denominationLegale>${titulaires_denominationSociale}</titulaires_denominationLegale>" >> $xml
         echo "  </marche>" >> $xml
     fi
 done < $csv
